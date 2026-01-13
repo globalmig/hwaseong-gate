0d9cb77e7aa2173be92ecd9e544281cdf9e0f560
globalmig <wdpb@mig.kr>
2026-01-13 11:21:08 +0900

    add: keywords

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,11 +8,23 @@ import Footer from "@/components/Footer";
 import Cs from "@/components/Cs";
 
 export const metadata: Metadata = {
-  title: '화성게이트 | 공장·상가·창고 공사 맞춤 철 구조물 제작 및 시공',
-  description: '홀딩도어와 양개도어, EGI 휀스 제작 및 시공 전문, 상담 010-2395-1592',
+  title: '화성게이트 | 공장·창고·상가 철구조물 제작·시공 전문',
+  description: '홀딩도어·양개도어·EGI 휀스 제작 및 시공 전문. 공장·창고·상가 맞춤 공사 상담 010-2395-1592',
+  keywords: [
+  "화성게이트",
+  "전국 철구조물",
+  "전국 공장 공사",
+  "홀딩도어",
+  "양개도어",
+  "EGI 휀스",
+  "창고 공사",
+  "상가 공사",
+  "철 구조물 제작",
+  "철 구조물 시공",
+],
   openGraph: {
     title: '화성게이트',
-    description: '홀딩도어와 양개도어, EGI 휀스 제작 및 시공 전문, 상담 010-2395-1592',
+    description: '홀딩도어·양개도어·EGI 휀스 제작 및 시공 전문. 공장·창고·상가 맞춤 공사 상담 010-2395-1592',
     url: 'https://www.hwasung-gate.com',
     siteName: '화성게이트',
     images: [
